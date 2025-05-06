175528a6fc4c2a095b11f328068829d97809e49f
ixand <hlukhovdima@gmail.com>
2025-05-06 16:52:03 +0300

    how it gets here

--- backend/models/order_item.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type OrderItem struct {
-	ID        uint    `json:"id" gorm:"primaryKey"`
-	OrderID   uint    `json:"order_id"`
-	ProductID uint    `json:"product_id"`
-	Quantity  int     `json:"quantity"`
-	UnitPrice float64 `json:"unit_price"`
-}
