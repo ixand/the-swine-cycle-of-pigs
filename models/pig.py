7f4806353b213f9aecd913396051e411b06c6269
ixand <hlukhovdima@gmail.com>
2025-05-15 15:32:44 +0300

    v 1.4t

--- models/pig.py
+++ models/pig.py
@@ -1,21 +1,22 @@
 from dataclasses import dataclass, field
 from datetime import datetime
+import random
 
 @dataclass
 class Pig:
     user_id: int
     name: str = "Безіменний"
-    weight: int = 1
-    mind: int = 1
-    strength: int = 1
-    health: int = 100
-    max_health: int = 100
+    weight: int = random.randint(1,5)
+    mind: int = random.randint(1,5)
+    strength: int = random.randint(1,5)
+    max_health: int = random.randint(10,15) * 10
+    health: int = max_health
     level: int = 1
-    xp: int = 1
-    gold: int = 0  # нове поле для нагород з квестів
+    xp: int = 0
+    gold: int = 0
     feeds_today: int = 0
     last_feed_time: str = ""
     fights_today: int = 0
     last_fight_date: str = ""
-    last_quest_time: str = ""  # додали нове поле для часу останнього квесту
+    last_quest_date: str = ""
     last_mining_time: str = ""
